bf9d9b65d962ddd3065dfc1d09467eabaa957c6a
Long Yu Wang <mikewangly@gmail.com>
2018-08-11 03:18:48 -0700

    servers up running on aws

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "fellow_name" {}
+
+/*variable "HOME" {
+  description = "Home directory in machine"
+}
+
+variable "project_home" {
+  description = "Path to root directory of this project"
+}*/
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+#variable "DOCKERHUB_USER" {}
+#variable "DOCKERHUB_PASSWD" {}
